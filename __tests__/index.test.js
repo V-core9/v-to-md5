dc1f92c7f5e030aa36eb9e184d16b8ba72d8d7ef
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-17 12:27:51 +0100

    yea works and some polishing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-const v_to_md5 = require("../source/v_to_md5");
+const v_to_md5 = require("../index");
 
 test("creates md5 hash from string 'demo_password_123456' ", () => {
   expect(v_to_md5("demo_password_123456")).toBe("b9a0dc00aff156a2a5e915ba00a590ba");
