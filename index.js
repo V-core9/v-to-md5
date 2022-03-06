6d589e9637f54a4836c94a6af28891fee4f70fd9
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-03-06 17:02:32 +0100

    yup making it much smaller

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const v_to_md5 = require("./src");
+var crypto = require("crypto");
+const { notEmpty } = require("v_is_empty_value");
+
+const v_to_md5 = async (value = null) => {
+  return (await notEmpty(value)) ? crypto.createHash("md5").update(String(value)).digest("hex") : false;
+};
 
 module.exports = v_to_md5;
--- src/index.js
+++
@@ -1,8 +0,0 @@
-var crypto = require("crypto");
-const { notEmpty } = require("v_is_empty_value");
-
-const v_to_md5 = async (value = null) => {
-  return (await notEmpty(value)) ? crypto.createHash("md5").update(String(value)).digest("hex") : false;
-};
-
-module.exports = v_to_md5;
